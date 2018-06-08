255f63cb55d000fe2c727f554ca5e36a6bf48ba0
Coelacanth7 <galford151@gmail.com>
2018-06-07 23:13:21 -0700

    working activities

--- App.js
+++ App.js
@@ -1,15 +1,84 @@
 import React from "react";
-import { StyleSheet, Text, ScrollView } from "react-native";
+import {
+	StyleSheet,
+	Text,
+	ScrollView,
+	View,
+	ActivityIndicator
+} from "react-native";
+
+const baseURI =
+	"https://maps.googleapis.com/maps/api/place/nearbysearch/json?radius=15000&location=";
+// "-33.8670522,151.1957362&key=AIzaSyCv4W7PfNeYhxsYnlU8evth0k-slUQeGk0";
+const key = "AIzaSyCv4W7PfNeYhxsYnlU8evth0k-slUQeGk0";
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			latitude: null,
+			longitude: null,
+			locations: [],
+			arrPos: 0,
+			error: null
+		};
+
+		this._onTapDo = this._onTapDo.bind(this);
+	}
+
+	getThatLocation() {
+		return new Promise((resolve, reject) => {
+			navigator.geolocation.getCurrentPosition(
+				position => resolve(position),
+				error => reject(error),
+				{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+			);
+		});
+	}
+
+	componentDidMount() {
+		this.getThatLocation()
+			.then(pos => {
+				let latt = pos.coords.latitude;
+				let long = pos.coords.longitude;
+				return fetch(`${baseURI}${latt},${long}&key=${key}`);
+			})
+			.then(res => res.json())
+			.then(json => {
+				let arr = json.results.map(place => place.name);
+				this.setState({ locations: arr });
+			})
+			.catch(e => console.error(e));
+	}
+
+	_onTapDo() {
+		this.setState((prevState, props) => {
+			let arrPos =
+				prevState.arrPos === prevState.locations.length - 1
+					? 0
+					: prevState.arrPos + 1;
+			return { arrPos };
+		});
+	}
+
 	render() {
-		console.log(navigator.geolocation.getCurrentPosition());
+		let { locations, arrPos } = this.state;
+
+		let thing = locations.length ? (
+			<Text style={styles.font}>do {locations[arrPos]}</Text>
+		) : (
+			<ActivityIndicator size="small" color="#fff" />
+		);
+
 		return (
-			<ScrollView style={styles.container}>
+			<View style={styles.container}>
 				<Text style={styles.font}>what do?</Text>
-				<Text style={styles.font}>{JSON.stringify(this.props, 0, 2)}</Text>
-				{/* <Text style={styles.font}>{JSON.stringify(this.state,0,2)}</Text> */}
-			</ScrollView>
+				{thing}
+				<Text style={styles.font} onPress={this._onTapDo}>
+					{locations.length > 0 && "no"}
+				</Text>
+			</View>
 		);
 	}
 }
@@ -18,13 +87,11 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: "black",
-		// alignItems: "center",
-		// justifyContent: "flex-start",
 		padding: 40
 	},
 	font: {
 		color: "#fff",
-		fontSize: 30,
+		fontSize: 50,
 		fontWeight: "bold"
 	}
 });
